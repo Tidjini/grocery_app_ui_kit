a2905cf1660d53fc592fd821ee3affe4a3c2be03
Tidjini_Messaoudi <dy.31@hotmail.fr>
2018-04-18 16:16:24 +0100

    Add cart item (init mode)

--- src/components/pieces/cartItem.js
+++ src/components/pieces/cartItem.js
@@ -2,3 +2,19 @@ import React, { Component } from "react";
 import { Image, Text, View } from "react-native";
 
 import Counter from "../common/counter";
+import colors from "../../../assets/colors";
+
+const { width, height } = Dimensions.get("window");
+
+export default class CartItem extends Component {
+  render() {
+    <View
+      style={{
+        width,
+        height: 80,
+        borderBottomWidth: 1,
+        borderColor: colors.dimGray
+      }}
+    />;
+  }
+}
--- src/components/scenes/cart.js
+++ src/components/scenes/cart.js
@@ -1,12 +1,26 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { View, Text, Image, Dimensions, TouchableOpacity } from "react-native";
 
 import { Header } from "../common";
 import colors from "../../../assets/colors";
 
+const { width, height } = Dimensions.get("window");
+const itemWidth = width / 2 - 10;
+const images = [
+  require("../../../assets/thumbnails/itemx01.jpg"),
+  require("../../../assets/thumbnails/itemx02.jpg"),
+  require("../../../assets/thumbnails/itemx03.jpg"),
+  require("../../../assets/thumbnails/itemx04.jpg"),
+  require("../../../assets/thumbnails/itemx05.jpg"),
+  require("../../../assets/thumbnails/itemx06.jpg"),
+  require("../../../assets/thumbnails/itemx07.jpg"),
+  require("../../../assets/thumbnails/itemx08.jpg"),
+  require("../../../assets/thumbnails/itemx09.jpg"),
+  require("../../../assets/thumbnails/itemx10.jpg")
+];
 export default class Cart extends Component {
   render() {
-    const { container } = styles;
+    const { container, imageContainer, image } = styles;
     return (
       <View style={container}>
         <Header primaryIcon="close" secondaryIcon="question" thirdIcon="gear">
@@ -37,11 +51,37 @@ export default class Cart extends Component {
             GRATUITE
           </Text>
         </View>
+
+        <View
+          style={{
+            width,
+            height: 80,
+            borderBottomWidth: 1,
+            borderColor: colors.davyGray,
+            backgroundColor: colors.white,
+            flexDirection: "row",
+            justifyContent: "space-between"
+          }}
+        >
+          <TouchableOpacity style={imageContainer}>
+            <Image source={images[5]} style={image} />
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
 }
 
 const styles = {
-  container: { flex: 1, backgroundColor: colors.whiteYellow }
+  container: { flex: 1, backgroundColor: colors.whiteYellow },
+  imageContainer: {
+    width: itemWidth,
+    height: 80,
+    paddingHorizontal: 10,
+    justifyContent: "center"
+  },
+  image: {
+    flex: 0.6,
+    resizeMode: "contain"
+  }
 };
